fc781824475e895908795efeb0bb648c5e9eda42
Sahana-balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-10-20 20:09:00 +0530

    Add files via upload

---
+++ data2.java
@@ -0,0 +1,41 @@
+public class Data2
+{
+	public static void main(String[] args)
+	{
+		byte earth;
+		earth=1;
+		System.out.println(earth);
+		
+		short minutesInDay;
+		minutesInDay=1440;
+		System.out.println(minutesInDay);
+		
+		int totalcovidcases;
+		totalcovidcases=1500000;
+		System.out.println(totalcovidcases);
+		
+		long areaofearth;
+		areaofearth=146000527;
+		System.out.println(areaofearth);
+		
+		float percentageIndegree;
+		percentageIndegree=93.28f;
+		System.out.println(percentageIndegree);
+		
+		double speedoflight;
+		speedoflight=300000000;
+		System.out.println(speedoflight);
+		
+		boolean sunIsPlanet;
+		sunIsPlanet=true;
+		System.out.println(sunIsPlanet);
+		
+		char grade;
+		grade='C';
+		System.out.println(grade);
+		
+		String name;
+		name="xworkz";
+		System.out.println(name);
+	}
+}
