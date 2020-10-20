4add80c65cd808fc1541fc638640522026401446
Sahana-balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-10-20 19:46:04 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,41 @@
+public class Data
+{
+	public static void main(String[] ards)
+	{
+		byte eyes;
+		eyes=2;
+		System.out.println("eyes");
+		
+		short daysInYear;
+		daysInYear=365;
+		System.out.println("daysInYear");
+		
+		int numofEngineersInIndia;
+		numofEngineersInIndia=1000000;
+		System.out.println("numofEngineersInIndia");
+		
+		long populationOfIndia;
+		populationOfIndia=1366000000;
+		System.out.println("populationOfIndia");
+		
+		float percentageInPUC;
+		percentageInPUC=85.5f;
+		System.out.println("percentageInPUC");
+		
+		double chargeOfElectron;
+		chargeOfElectron=-1.6021766200000000000;
+		System.out.println("chargeOfElectron");
+		
+		boolean sunIsStar;
+		sunIsStar=true;
+		System.out.println("sunIsStar");
+		
+		char grade;
+		grade='A';
+		System.out.println("grade");
+		
+		String name;
+		name="sunny";
+		System.out.println("name");
+	}
+}
