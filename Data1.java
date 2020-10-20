e3d37a15e5b1f3cbe4936a9670a15cc52fad24d5
Sahana-balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-10-20 19:46:40 +0530

    Add files via upload

---
+++ Data1.java
@@ -0,0 +1,41 @@
+public class Data1
+{
+	public static void main(String[] ards)
+	{
+		byte nose;
+		nose=1;
+		System.out.println("nose");
+		
+		short hoursInDay;
+		hoursInDay=24;
+		System.out.println("hoursInDay");
+		
+		int ITemployeesInKarnataka;
+		ITemployeesInKarnataka=1500000;
+		System.out.println("ITemployeesInKarnataka");
+		
+		long areaofmoon;
+		areaofmoon=146000527;
+		System.out.println("areaofmoon");
+		
+		float percentageInSSLC;
+		percentageInSSLC=93.28f;
+		System.out.println("percentageInSSLC");
+		
+		double speedoflight;
+		speedoflight=300000000;
+		System.out.println("speedoflight");
+		
+		boolean sunIsPlanet;
+		sunIsPlanet=true;
+		System.out.println("sunIsPlanet");
+		
+		char grade;
+		grade='B';
+		System.out.println("grade");
+		
+		String name;
+		name="munny";
+		System.out.println("name");
+	}
+}
