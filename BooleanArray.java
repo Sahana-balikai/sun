4b5ceffe9ea6d6a22445db688a2759e4e2de8230
Sahana-balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-10-21 21:03:01 +0530

    Add files via upload

---
+++ BooleanArray.java
@@ -0,0 +1,12 @@
+public class BooleanArray
+{
+  public static void main(String args[])
+  {
+    boolean array[] = {false,false,true,true};
+    System.out.println("Java boolean Array Example");
+    for(int i=0;i<array.length;i++)
+    {
+      System.out.println("boolean array Element at : "+ i + " " + array[i]);
+    }
+  }
+}
