41e44485352ab729813261debf606877db67a76e
Sahana-balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-10-22 22:37:59 +0530

    Add files via upload

---
+++ States.java
@@ -0,0 +1,29 @@
+public class States{		
+public static void main(String[] names){		
+System.out.println("JVM invokes main method");
+
+int countOfStates=names.length;
+System.out.println("Count of states: "+countOfStates);
+	
+System.out.println("State name at index 0 "+names[0]);
+System.out.println("State name at index 1 "+names[1]);
+System.out.println("State name at index 2 "+names[2]);
+System.out.println("State name at index 3 "+names[3]);
+System.out.println("State name at index 4 "+names[4]);
+System.out.println("State name at index 5 "+names[5]);
+System.out.println("State name at index 6 "+names[6]);
+System.out.println("State name at index 7 "+names[7]);
+System.out.println("State name at index 8 "+names[8]);
+System.out.println("State name at index 9 "+names[9]);
+System.out.println("State name at index 10 "+names[10]);
+System.out.println("State name at index 11 "+ names[11]);
+System.out.println("State name at index 12 "+names[12]);
+System.out.println("State name at index 13 "+names[13]);
+System.out.println("State name at index 14 "+names[14]);
+System.out.println("State name at index 15 "+names[15]);
+System.out.println("State name at index 16 "+names[16]);
+System.out.println("State name at index 17 "+names[17]);
+
+System.out.println("exit from main");
+}
+}
