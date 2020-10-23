96b585668a00a7905c82e27d2a58aca1ad38a63f
Sahana Balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-10-23 22:17:51 +0530

    Add files via upload

---
+++ Live.java
@@ -0,0 +1,13 @@
+public class Live
+{
+	public static void main(String[] sahana)
+	{	
+      System.out.println("Invoke main method by JVM");
+      
+	Life.work(sahana[0]);
+		
+     System.out.println("Exit of main method by JVM");
+		
+	}
+
+}
