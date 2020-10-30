f206e8025c6604c5868325bb974c2cfcda50b3d6
Sahana Balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-10-30 08:27:34 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,24 @@
+public class Library
+{
+	public static void subject(String nameOfBook)
+	{
+		
+		System.out.println("name of the book: "+ nameOfBook);
+				
+	}	
+	
+	public static void subject(int numberOfBooks)
+	{
+		
+		System.out.println("number of the book: "+ numberOfBooks);
+				
+	}
+	public static void subject(int numberOfStudents,int numberOfTeachers)
+	{
+		
+		System.out.println("The number of students are: "+ numberOfStudents);
+		System.out.println("The number of nurses are: "+ numberOfTeachers);
+				
+	}
+		
+}
