17edd4743f98a31d999abcfde82f00ddf2eb79e4
Sahana Balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-10-30 08:31:45 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,24 @@
+public class Car
+{
+	public static void Model(String nameOfCar)
+	{
+		
+		System.out.println("name of the car: "+ nameOfCar);
+				
+	}	
+	
+	public static void Model(int numberOfCars)
+	{
+		
+		System.out.println("number of the cars: "+ numberOfCars);
+				
+	}
+	public static void Model(int numberOfSeats,String numberOfModel)
+	{
+		
+		System.out.println("The number of seats are: "+ numberOfSeats);
+		System.out.println("The number of model is: "+ numberOfModel);
+				
+	}
+		
+}
