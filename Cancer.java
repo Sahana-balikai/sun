a4b72d0893df116a14d2764677d65f4633159098
Sahana Balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-10-30 08:30:58 +0530

    Add files via upload

---
+++ Cancer.java
@@ -0,0 +1,24 @@
+public class Cancer
+{
+	public static void type(String nameOfCancer)
+	{
+		
+		System.out.println("name of the cancer: "+ nameOfCancer);
+				
+	}	
+	
+	public static void type(int numberOfCancerPatients)
+	{
+		
+		System.out.println("number of the cancer patients: "+ numberOfCancerPatients);
+				
+	}
+	public static void type(int typesOfCancer,int typesOfTreatment)
+	{
+		
+		System.out.println("The type of cancer are: "+ typesOfCancer);
+		System.out.println("The type of treatment are: "+ typesOfTreatment);
+				
+	}
+		
+}
