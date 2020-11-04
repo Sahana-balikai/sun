86d93b7333a520d043681ebfdb99be0d033c377f
Sahana Balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-11-04 21:58:29 +0530

    Add files via upload

---
+++ Nation.java
@@ -0,0 +1,15 @@
+public class Nation
+{
+	public static void main(String[] country)
+	{
+		System.out.println("JVM invokes main");
+		
+		System.out.println("size of names: "+country.length);
+				
+		for(int print=0;print<country.length;print++)
+		{
+		System.out.println("The country name at index "+print + " is :"+ country[print]);
+		
+		}
+	}
+}
