5d1d94c13c3b3f97b32cef086d99bd40a9dc0225
Sahana Balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-11-17 03:01:22 -1200

    Add files via upload

---
+++ NumberSearch.java
@@ -0,0 +1,9 @@
+package com.sahana.basics;
+
+public class NumberSearch {
+	public static void main(String[] args) {
+
+		System.out.println("Total items to search" + PhoneNumbers.totalItems());
+		PhoneNumbers.search(9980297429l);
+	}
+}
