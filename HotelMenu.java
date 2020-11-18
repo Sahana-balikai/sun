38a3ad90c0b11d00fbf69470fdbba32f4670452e
Sahana Balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-11-17 15:00:35 -1200

    Delete HotelMenu.java

--- HotelMenu.java
+++
@@ -1,9 +0,0 @@
-package com.sahana.basics;
-
-public class HotelMenu {
-	public static void main(String[] args) {
-
-		System.out.println("Total items to search" + Order.totalItems());
-		Order.search("Idli");
-	}
-}
