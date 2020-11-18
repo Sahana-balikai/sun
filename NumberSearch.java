876a24b3ed8952afc6fc292ddf47471bcfed350e
Sahana Balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-11-17 15:00:50 -1200

    Delete NumberSearch.java

--- NumberSearch.java
+++
@@ -1,9 +0,0 @@
-package com.sahana.basics;
-
-public class NumberSearch {
-	public static void main(String[] args) {
-
-		System.out.println("Total items to search" + PhoneNumbers.totalItems());
-		PhoneNumbers.search(9980297429l);
-	}
-}
