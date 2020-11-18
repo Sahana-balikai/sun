300e0e74c538a156de909de3a1b8fb879d1d7af8
Sahana Balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-11-17 15:00:20 -1200

    Delete BusSearch.java

--- BusSearch.java
+++
@@ -1,9 +0,0 @@
-package com.sahana.basics;
-
-public class BusSearch {
-	public static void main(String[] args) {
-
-		//System.out.println("Total items to search" + Place.totalItems());
-		Place.search("Hubli");
-	}
-}
