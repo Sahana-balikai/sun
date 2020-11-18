63289b525b5208b7d8e5d60a6ecc79b04c8e91eb
Sahana Balikai <72904718+Sahana-balikai@users.noreply.github.com>
2020-11-17 15:04:00 -1200

    Add files via upload

---
+++ BusSearch.java
@@ -0,0 +1,9 @@
+package com.sahana.basics.Bus;
+
+public class BusSearch {
+	public static void main(String[] args) {
+
+		//System.out.println("Total items to search" + Place.totalItems());
+		Place.search("Hubli");
+	}
+}
